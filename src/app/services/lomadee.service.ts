db724441199c6393553b00061d0cf080fb8109c5
CABRALBRUNO000 <cabralbruno000@gmail.com>
2024-12-13 21:31:38 -0300

    14

--- src/app/services/lomadee.service.ts
+++ src/app/services/lomadee.service.ts
@@ -8,7 +8,7 @@ import { CategoryResponse } from '../interfaces/category.interface';
   providedIn: 'root'
 })
 export class LomadeeService {
-  private readonly API_URL = '/api';  // Alterado de https://api.lomadee.com/v3 para /api
+  private readonly API_URL = 'https://api.lomadee.com/v3';  // Alterado de https://api.lomadee.com/v3 para /api
   private readonly APP_TOKEN = '1733757952030999173a0';
   private readonly SOURCE_ID = '38322289';
   private readonly SORT = 'discount';
